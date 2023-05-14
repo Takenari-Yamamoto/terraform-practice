e0e140a5aa515fab15c4e079c50bad7bfcd3c171
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-05-14 10:01:10 +0900

    fix

--- 001_terraform_gcp/main.tf
+++ 001_terraform_gcp/main.tf
@@ -2,15 +2,17 @@ provider "google" {
   credentials = file(var.credentials)
   project     = var.project_id
   region      = "us-central1"
+  zone        = "us-central1-a"
 }
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
@@ -22,3 +24,4 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
