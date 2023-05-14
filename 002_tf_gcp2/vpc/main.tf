75e5ea69967bf477434fb833e46aaa46aa8cda4e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-05-14 16:35:49 +0900

    fix

--- 002_tf_gcp2/vpc/main.tf
+++ 002_tf_gcp2/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc" {
   name                    = "my-tf-vpc"
-  project                 = "golang-project-377611"
+  project                 = var.project_id
   auto_create_subnetworks = false
 }
