da503954a0b4bc4e9c0d66d57f738abefcfdd5da
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-06-10 19:15:25 +0900

    code

---
+++ 004_official_tutorial/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/Users/takenariyamamoto/Downloads/golang-project-377611-721f9059e7d8.json")
+
+  project = "golang-project-377611"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
